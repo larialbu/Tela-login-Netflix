09f464286a0bc4ab1b6863db681101ce769a6e17
Larissa Albuquerque <105246799+larialbu@users.noreply.github.com>
2022-07-28 17:53:05 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,8 @@
+function buscar(){
+    var nome = document.getElementById('nome').value;
+
+    // var paragrafo = document.getElementById('paragrafo');
+    // paragrafo.setAttribute("value", `${nome}, seu contato foi recebido com sucesso!`)
+
+    alert(`${nome}, seu contato foi recebido com sucesso!`)
+}
